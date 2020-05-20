f3aaf7296cd4ff9bb0acd3c000dac3f3d2e692f3
venky12131 <51823743+venky12131@users.noreply.github.com>
2020-05-20 11:14:45 +0530

    Create heart-beat-hotelApplicationTests.java

---
+++ src/main/java/com/test/websocket/model/heart-beat-hotelApplicationTests.java
@@ -0,0 +1,9 @@
+package com.myProjects.heart-beat-hotel;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class heart-beat-hotelApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
