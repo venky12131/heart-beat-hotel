32344749a566af5614e3f3db5c03e63ecdb45dff
venky12131 <51823743+venky12131@users.noreply.github.com>
2020-05-20 11:18:41 +0530

    Delete heart-beat-hotelApplicationTests.java

--- src/main/java/com/test/websocket/model/heart-beat-hotelApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.myProjects.heart-beat-hotel;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTest
-class heart-beat-hotelApplicationTests {
-	@Test
-	void contextLoads() {
-	}
-}
